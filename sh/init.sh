34a8369d6a6289ab75e9a22c60a695457a98aa3a
尤雨东 <root@ltpp.vip>
2025-02-04 10:01:36 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/VUE-EXE.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/VUE-EXE.git;
 git remote add github git@github.com:ltpp-universe/VUE-EXE.git;
 git remote add origin git@github.com:ltpp-universe/VUE-EXE.git;
